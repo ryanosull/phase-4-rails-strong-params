b03ac7ed61c61fae27f98b7c561cc7bc0c0e54f9
Ryan <wrosullivan88@gmail.com>
2023-01-13 15:12:13 -0600

    complete

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -7,8 +7,19 @@ class BirdsController < ApplicationController
   end
 
   # POST /birds
+
+  # def create
+  #   bird = Bird.create(name: params[:name], species: params[:species])
+  #   render json: bird, status: :created
+  # end
+
+  # def create
+  #   bird = Bird.create(params)
+  #   render json: bird, status: :created
+  # end
+
   def create
-    bird = Bird.create(name: params[:name], species: params[:species])
+    bird = Bird.create(params.permit(:name, :species))
     render json: bird, status: :created
   end
 
